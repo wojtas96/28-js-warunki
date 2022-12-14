ff3a65b914bb348b6fcfcc877434966b54eb2a33
Klaudia <koliber96k@gmail.com>
2022-12-14 13:14:36 +0100

    28-js-warunki

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let number = 12;
+console.log(number);
+
+if (number > 10) {
+    console.log('Podana liczba jest większa od 10');
+} else {
+    console.log('Podana liczba nie jest większa od 10');
+}
